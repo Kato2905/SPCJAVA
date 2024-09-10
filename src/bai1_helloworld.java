dd5039c03724d8d13856adaea415994a5ac4d43a
Kato2905 <raidenshogun29052007@gmail.com>
2024-09-10 08:35:06 +0700

    the gioi dia nguc

---
+++ src/bai1_helloworld.java
@@ -0,0 +1,17 @@
+
+import javax.crypto.spec.PSource;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class bai1_helloworld {
+    public static void main(String[] args) {
+            System.out.println("Hello World!");
+    }
+}
