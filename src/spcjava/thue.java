06bff5ec37ee606fb9f2307fbe321e99a4461131
Kato2905 <raidenshogun29052007@gmail.com>
2024-09-17 11:29:07 +0700

    ASD

---
+++ src/spcjava/thue.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spcjava;
+import java.util.Scanner;
+/**
+ *
+ * @author Administrator
+ */
+public class thue {
+    public static void main(String[] args) {
+        System.out.println("CHUONG TRINH TINH THUE THU NHAP");
+        double luong, thuong , thue;
+        Scanner sc = new Scanner(System.in);
+        luong = sc.nextDouble();
+        thuong = sc.nextDouble();
+        thue = sc.nextDouble();
+        if(luong + thuong < 9000000 ){
+            System.out.println("Khong tinh thue");
+            else{
+                    if(luong + thuong <= 15000000)
+                    }
+        
+       
+        }
+       
+    }
+
+}
