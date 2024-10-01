a1ee5b279b8bf75027b1d298030e9b7f90e0fe83
Kato2905 <Administrator@LAB1-MAY29>
2024-10-01 16:36:45 +0700

    hinhvuong

---
+++ src/BaiHinhHoc/hinhVuong.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BaiHinhHoc;
+
+/**
+ *
+ * @author Administrator
+ */
+public class hinhVuong extends HinhChuNhat{
+    public hinhVuong(){
+        super();
+        
+    }
+    public hinhVuong(int doRong){
+        super(doRong, doRong);
+    }
+}
