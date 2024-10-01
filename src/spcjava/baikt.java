c457fd8f9fcd069e4e6220b198eec4af8bfc5982
Kato2905 <Administrator@LAB1-MAY29>
2024-10-01 10:58:24 +0700

    kt cau 1

---
+++ src/spcjava/baikt.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spcjava;
+import java.awt.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class baikt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String i;
+         int so;
+        System.out.println("Nhap so (nhap 'stop' den dung)");
+        while(true){
+            i = sc.nextLine();
+            if(i.equalsIgnoreCase("stop"))
+                break;
+        }
+     
+        
+        
+    }
+}
+
+        
+ 
