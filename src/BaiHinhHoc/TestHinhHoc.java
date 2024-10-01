71e34c7ed683ed230c0a178a01bd5e4c67493943
Kato2905 <Administrator@LAB1-MAY29>
2024-10-01 16:36:56 +0700

    testhinhhoc

---
+++ src/BaiHinhHoc/TestHinhHoc.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BaiHinhHoc;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestHinhHoc {
+    public static void main(String[] args) {
+        HinhHoc h;
+        h = new HinhChuNhat(4,5);
+        System.out.println("Chu Vi= "+ h.tinhChuVi());
+    }
+    
+}
